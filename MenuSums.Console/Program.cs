7e9d6acacdb94d3a26588d69a140f246bd45dc73
Derek Morrison <me@derek-morrison.com>
2020-09-10 14:15:58 -0500

    Tweak name of console app

---
+++ MenuSums.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MenuSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                // TODO: Use more specific exception type here?
+                throw new Exception("No input file given.");
+            }
+
+            Console.WriteLine($"First arg: {args[0]}");
+        }
+    }
+}
