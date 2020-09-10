10e47586ab17e0a90a33d6ba4f46f1f1ea435af4
Derek Morrison <me@derek-morrison.com>
2020-09-10 16:37:34 -0500

    Get basic program working

--- MenuSums.Console/Program.cs
+++ MenuSums.Console/Program.cs
@@ -1,18 +1,50 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 
 namespace MenuSums
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             if (args.Length == 0)
             {
-                // TODO: Use more specific exception type here?
-                throw new Exception("No input file given.");
+                Console.WriteLine("No input file given.");
+
+                return 1;
             }
 
-            Console.WriteLine($"First arg: {args[0]}");
+            var fileName = args[0];
+
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("Input file given was not found.", fileName);
+            }
+
+            using var fileStream = File.OpenRead(fileName);
+            using var document = JsonDocument.Parse(fileStream);
+
+            foreach (var element in document.RootElement.EnumerateArray())
+            {
+                var count = 0;
+                var items = element.GetProperty("menu").GetProperty("items");
+
+                foreach (var item in items.EnumerateArray())
+                {
+                    if (item.ValueKind == JsonValueKind.Null) continue;
+
+                    JsonElement label;
+                    if (!item.TryGetProperty("label", out label)) continue;
+
+                    var id = item.GetProperty("id").GetInt32();
+                    count += id;
+                }
+
+                Console.WriteLine(count);
+            }
+
+            return 0;
         }
     }
 }
