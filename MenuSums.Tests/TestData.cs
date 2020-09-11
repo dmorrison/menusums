44a89481a57f13dbc94ae38056486ecfe8e5ac33
Derek Morrison <me@derek-morrison.com>
2020-09-10 23:25:37 -0500

    Fill in a few tests with data

---
+++ MenuSums.Tests/TestData.cs
@@ -0,0 +1,57 @@
+﻿
+namespace MenuSums.Tests
+{
+    // TODO: Brainstorm on how to specify a variety of test data in a (possibly)
+    // more readable way.
+    public static class TestData
+    {
+        public static string NoItems
+        {
+            get
+            {
+                return @"
+[
+    {
+        ""menu"": {
+            ""header"": ""menu"",
+            ""items"": []
+        }
+    }
+]";
+            }
+        }
+
+        public static string WithNulls
+        {
+            get
+            {
+                return @"
+[
+    {
+        ""menu"": {
+            ""header"": ""menu"",
+            ""items"": [
+                {
+                    ""id"": 27
+                },
+                {
+                    ""id"": 0,
+                    ""label"": ""Label 0""
+                },
+                null,
+                {
+                    ""id"": 93
+                },
+                null,
+                {
+                    ""id"": 46,
+                    ""label"": ""Label 46""
+                }
+            ]
+        }
+    }
+]";
+            }
+        }
+    }
+}
--- MenuSums.Tests/Tests.cs
+++ MenuSums.Tests/Tests.cs
@@ -1,13 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MenuSums;
 
 namespace MenuSums.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Returns_sum_0_when_no_menu_items()
         {
+            var results = MenuSummer.CalculateSums(TestData.NoItems);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(0, results[0]);
+        }
+
+        [TestMethod]
+        public void Can_handle_null_menu_items()
+        {
+            var results = MenuSummer.CalculateSums(TestData.WithNulls);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(46, results[0]);
         }
     }
 }
